cc7e444c513c0fb3c5073cad77314640ffa0995c
Micky Chen <mickychen0524@yahoo.com>
2021-05-17 22:51:46 -0400

    initial project setup

---
+++ next.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+  future: {
+    webpack5: true,
+  },
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  trailingSlash: true,
+  webpackDevMiddleware: config => {
+    const newConfig = config;
+    newConfig.watchOptions = {
+      aggregateTimeout: 300,
+      poll: 1000,
+    };
+    return newConfig;
+  },
+};
---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Casper Association Portal</title>
+        <meta name="description" content="Casper Association Portal" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
