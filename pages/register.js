4911a662da59e2675f6c81a7fa306919089d57ff
Micky Chen <mickychen0524@yahoo.com>
2021-05-20 06:32:15 -0400

    register mobile responsive

--- pages/register.js
+++ pages/register.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import Image from 'next/image';
 import AppFooter from '../components/Layouts/app-footer';
 import AppHeader from '../components/Layouts/app-header';
 
@@ -33,98 +32,99 @@ const Register = () => {
 
   return (
     <div className="flex justify-center min-h-screen">
-      <div className="md:w-9/12 p-9 flex flex-col">
+      <div className="w-full md:max-w-screen-2xl md:p-9 p-4 flex flex-col">
         <AppHeader theme="dark" />
-        <div className="flex-grow flex items-center justify-center">
-          <div className="md:w-9/12">
+        <div className="flex-grow flex items-center justify-center mt-16 md:mt-0">
+          <div className="w-full md:w-9/12">
             <p className="text-2xl">New User</p>
             <p className="text-sm text-dark mt-2">
               Fill out the form to register.
             </p>
-            <div className="flex mt-10 space-x-5">
+            <div className="md:flex mt-10 md:space-x-5">
               <input
                 type="text"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="First Name *"
                 value={firstName}
                 onChange={e => setFirstName(e.target.value)}
               />
               <input
                 type="text"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Last Name *"
                 value={lastName}
                 onChange={e => setLastName(e.target.value)}
               />
               <input
                 type="text"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Company/Organization *"
                 value={company}
                 onChange={e => setCompany(e.target.value)}
               />
             </div>
-            <div className="flex mt-5 space-x-5">
+            <div className="md:flex mt-5 md:space-x-5">
               <input
                 type="email"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Email *"
                 value={email}
                 onChange={e => setEmail(e.target.value)}
               />
               <input
                 type="email"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Confirm Email *"
                 value={confirmEmail}
                 onChange={e => setConfirmEmail(e.target.value)}
               />
-              <div className="flex-1 h-14 px-7" />
+              <div className="flex-1 h-0 md:h-14 px-7" />
             </div>
-            <div className="flex mt-5 space-x-5">
+            <div className="md:flex mt-5 md:space-x-5">
               <input
                 type="password"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Password *"
                 value={password}
                 onChange={e => setPassword(e.target.value)}
               />
               <input
                 type="password"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Confirm Password *"
                 value={confirmPassword}
                 onChange={e => setConfirmPassword(e.target.value)}
               />
-              <div className="flex-1 h-14 px-7" />
+              <div className="flex-1 h-0 md:h-14 px-7" />
             </div>
-            <div className="flex mt-5 space-x-5">
+            <div className="md:flex mt-5 md:space-x-5">
               <input
                 type="text"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Forum Name / Pseudonym *"
                 value={forumName}
                 onChange={e => setForumName(e.target.value)}
               />
               <input
                 type="text"
-                className="flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
                 placeholder="Telegram"
                 value={telegram}
                 onChange={e => setTelegram(e.target.value)}
               />
-              <div className="flex-1 h-14 px-7" />
+              <div className="flex-1 h-0 md:h-14 px-7" />
             </div>
-            <div className="flex items-center mt-10">
+            <div className="flex mt-10">
               <button
                 type="button"
-                className="flex items-center focus:outline-none"
+                className="flex items-baseline focus:outline-none"
                 onClick={() => setAgreeChecked(!agreeChecked)}
               >
-                <Image
+                <img
                   src={`/images/${
                     agreeChecked ? 'ic_check.svg' : 'ic_uncheck.svg'
                   }`}
+                  alt="agree checkbox"
                   width={18}
                   height={18}
                 />
@@ -137,13 +137,14 @@ const Register = () => {
             <div className="flex mt-5">
               <button
                 type="button"
-                className="focus:outline-none"
+                className="flex items-baseline focus:outline-none"
                 onClick={() => setUnderstandChecked(!understandChecked)}
               >
-                <Image
+                <img
                   src={`/images/${
                     understandChecked ? 'ic_check.svg' : 'ic_uncheck.svg'
                   }`}
+                  alt="understand checkbox"
                   width={18}
                   height={18}
                 />
@@ -154,10 +155,10 @@ const Register = () => {
                 that I will be required to prove I am a node operator.
               </p>
             </div>
-            <div className="flex flex-row-reverse mt-10">
+            <div className="md:flex md:flex-row-reverse mt-10">
               <button
                 type="button"
-                className="text-lg text-white w-64 h-16 rounded-full bg-primary hover:opacity-40 disabled:opacity-40 disabled:cursor-not-allowed focus:outline-none"
+                className="text-lg text-white w-full md:w-64 h-16 rounded-full bg-primary hover:opacity-40 disabled:opacity-40 disabled:cursor-not-allowed focus:outline-none"
                 disabled={!validateFields()}
                 onClick={() => router.push('/welcome')}
               >
@@ -166,7 +167,7 @@ const Register = () => {
             </div>
           </div>
         </div>
-        <AppFooter />
+        <AppFooter theme="dark" />
       </div>
     </div>
   );
