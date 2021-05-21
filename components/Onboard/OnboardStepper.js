aa2135517be3aee0c0c028c4dc64821d9c0a9a4e
Micky Chen <mickychen0524@yahoo.com>
2021-05-21 12:00:30 -0400

    optimize onboard

---
+++ components/Onboard/OnboardStepper.js
@@ -0,0 +1,101 @@
+const OnboardStepper = ({
+  currentStep,
+  totalSteps,
+  stepContent,
+  showNextButton,
+  showContinueButton,
+  continueButtonTitle,
+  onPrev,
+  onNext,
+}) => (
+  <>
+    <div className="block md:hidden w-full flex justify-between">
+      <button
+        type="button"
+        className="flex items-center focus:outline-none"
+        onClick={onPrev}
+      >
+        <img
+          src="/images/ic_prev_circle.svg"
+          alt="prev"
+          width="18"
+          height="18"
+          className="mr-2"
+        />
+        Back
+      </button>
+      <button
+        type="button"
+        className={`flex items-center focus:outline-none ${
+          showNextButton ? 'visible' : 'invisible'
+        }`}
+        onClick={onNext}
+      >
+        Next
+        <img
+          src="/images/ic_next_circle.svg"
+          alt="next"
+          width="18"
+          height="18"
+          className="ml-2"
+        />
+      </button>
+    </div>
+    <div className="w-full md:w-9/12">
+      <p className="hidden md:block border-b border-gray pb-1 font-bold text-dark2">
+        Esign Terms
+      </p>
+      <div
+        className="hidden md:block border-b border-primary border-2"
+        style={{ width: `${(currentStep * 100) / totalSteps}%` }}
+      />
+      <div className="mt-2 md:flex md:space-x-12">
+        <div className="relative bg-white w-full md:w-auto">
+          <img
+            src="/images/img_signature_blur.png"
+            alt="esign terms"
+            className="w-full h-44 md:h-auto object-cover"
+          />
+          <div className="absolute bottom-0 mx-4 my-10 opacity-30">
+            <p className="text-2xl">Esign Terms</p>
+            <p className="text-sm text-dark1 mt-2">
+              You must read and agree to the terms of service before continuing
+              to the portal
+            </p>
+          </div>
+        </div>
+        {stepContent}
+      </div>
+      <div className="hidden md:flex justify-between border-b border-gray pb-2">
+        <button
+          type="button"
+          className="text-center ml-4 text-sm text-dark3 flex flex-col items-center justify-end focus:outline-none"
+          onClick={onPrev}
+        >
+          <img
+            src="/images/ic_prev_circle_gradient_small.svg"
+            alt="back"
+            className="mb-1"
+          />
+          Back
+        </button>
+        <button
+          type="button"
+          className={`text-center ml-5 text-sm text-dark3 focus:outline-none ${
+            showContinueButton ? 'opacity-100' : 'opacity-25'
+          }`}
+          onClick={onNext}
+        >
+          <img
+            src="/images/ic_next_circle_gradient_large.svg"
+            alt="go to esign"
+            className="mb-1"
+          />
+          {continueButtonTitle}
+        </button>
+      </div>
+    </div>
+  </>
+);
+
+export default OnboardStepper;
--- pages/onboard/esign-terms.js
+++ pages/onboard/esign-terms.js
@@ -4,6 +4,7 @@ import AppFooter from '../../components/Layouts/app-footer';
 import AppHeader from '../../components/Layouts/app-header';
 import EsignTermsFirstStep from '../../components/Onboard/EsignTerms/FirstStep';
 import EsignTermsSecondStep from '../../components/Onboard/EsignTerms/SecondStep';
+import OnboardStepper from '../../components/Onboard/OnboardStepper';
 
 const EsignTerms = () => {
   const [currentStep, setCurrentStep] = useState(1);
@@ -59,94 +60,16 @@ const EsignTerms = () => {
       <div className="w-full md:max-w-screen-2xl md:p-9 p-4 flex flex-col">
         <AppHeader theme="dark" />
         <div className="flex-grow md:flex md:items-center justify-center mt-12 md:mt-0">
-          <div className="block md:hidden w-full flex justify-between">
-            <button
-              type="button"
-              className="flex items-center focus:outline-none"
-              onClick={() => handlePrev()}
-            >
-              <img
-                src="/images/ic_prev_circle.svg"
-                alt="prev"
-                width="18"
-                height="18"
-                className="mr-2"
-              />
-              Back
-            </button>
-            <button
-              type="button"
-              className={`flex items-center focus:outline-none ${
-                currentStep !== totalSteps && selectedDocument
-                  ? 'visible'
-                  : 'invisible'
-              }`}
-              onClick={() => setCurrentStep(currentStep + 1)}
-            >
-              Next
-              <img
-                src="/images/ic_next_circle.svg"
-                alt="next"
-                width="18"
-                height="18"
-                className="ml-2"
-              />
-            </button>
-          </div>
-          <div className="w-full md:w-9/12">
-            <p className="hidden md:block border-b border-gray pb-1 font-bold text-dark2">
-              Esign Terms
-            </p>
-            <div
-              className="hidden md:block border-b border-primary border-2"
-              style={{ width: `${(currentStep * 100) / totalSteps}%` }}
-            />
-            <div className="mt-2 md:flex md:space-x-12">
-              <div className="relative bg-white w-full md:w-auto">
-                <img
-                  src="/images/img_signature_blur.png"
-                  alt="esign terms"
-                  className="w-full h-44 md:h-auto object-cover"
-                />
-                <div className="absolute bottom-0 mx-4 my-10 opacity-30">
-                  <p className="text-2xl">Esign Terms</p>
-                  <p className="text-sm text-dark1 mt-2">
-                    You must read and agree to the terms of service before
-                    continuing to the portal
-                  </p>
-                </div>
-              </div>
-              {renderStepContent()}
-            </div>
-            <div className="hidden md:flex justify-between border-b border-gray pb-2">
-              <button
-                type="button"
-                className="text-center ml-4 text-sm text-dark3 flex flex-col items-center justify-end focus:outline-none"
-                onClick={() => handlePrev()}
-              >
-                <img
-                  src="/images/ic_prev_circle_gradient_small.svg"
-                  alt="back"
-                  className="mb-1"
-                />
-                Back
-              </button>
-              <button
-                type="button"
-                className={`text-center ml-5 text-sm text-dark3 focus:outline-none ${
-                  selectedDocument ? 'opacity-100' : 'opacity-25'
-                }`}
-                onClick={() => handleNext()}
-              >
-                <img
-                  src="/images/ic_next_circle_gradient_large.svg"
-                  alt="go to esign"
-                  className="mb-1"
-                />
-                {getNextButtonTitle()}
-              </button>
-            </div>
-          </div>
+          <OnboardStepper
+            currentStep={currentStep}
+            totalSteps={totalSteps}
+            stepContent={renderStepContent()}
+            showNextButton={currentStep !== totalSteps && selectedDocument}
+            showContinueButton={!!selectedDocument}
+            continueButtonTitle={getNextButtonTitle()}
+            onPrev={handlePrev}
+            onNext={handleNext}
+          />
         </div>
         <AppFooter theme="dark" />
       </div>
