98b8b1371fc63f7e5e4f965538bb3a6505479a6d
LedgerLeapLLC <ledgerleapllc@gmail.com>
2021-06-07 10:48:14 -0400

    registration form validation

--- pages/register.js
+++ pages/register.js
@@ -1,56 +1,60 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { useForm, Controller } from 'react-hook-form';
 import AppFooter from '../components/layouts/app-footer';
 import AppHeader from '../components/layouts/app-header';
+import {
+  NAME_PATTERN,
+  EMAIL_PATTERN,
+  PASSWORD_PATTERN,
+  FORUM_PATTERN,
+  TELEGRAM_PATTERN,
+} from '../components/helpers/form_validation';
 
 const Register = () => {
-  const [firstName, setFirstName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [company, setCompany] = useState('');
-  const [email, setEmail] = useState('');
-  const [confirmEmail, setConfirmEmail] = useState();
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [forumName, setForumName] = useState('');
-  const [telegram, setTelegram] = useState('');
   const [agreeChecked, setAgreeChecked] = useState(false);
   const [understandChecked, setUnderstandChecked] = useState(false);
 
   const router = useRouter();
+  const {
+    control,
+    formState,
+    register,
+    handleSubmit,
+    getValues,
+    setValue,
+    clearErrors,
+    setError,
+  } = useForm({
+    mode: 'onBlur',
+  });
+  const onSubmit = data => router.push('/welcome');
 
   const validateFields = () => {
-    if (!firstName || firstName.trim() === '') {
-      return false;
-    }
-
-    if (!lastName || lastName.trim() === '') {
-      return false;
-    }
-
     if (!agreeChecked || !understandChecked) {
       return false;
     }
-
     return true;
   };
 
-  const handleTelegram = val => {
-    if (val === '' || !/^@/.test(val)) {
-      setTelegram('@');
-    } else {
-      setTelegram(val);
-    }
-  };
-
   const focusTelegram = e => {
-    if (e.target.value === '') {
-      setTelegram('@');
+    if (!e.target.value || e.target.value === '') {
+      setValue('telegram', '@');
     }
   };
 
   const blurTelegram = e => {
     if (e.target.value === '@') {
-      setTelegram('');
+      setValue('telegram', '');
+    }
+  };
+
+  const onChangeTelegram = e => {
+    if (e.target.value === '' || !/^@/.test(e.target.value)) {
+      setValue('telegram', '@');
+      e.target.value = '@';
+    } else {
+      setValue('telegram', e.target.value);
     }
   };
 
@@ -58,7 +62,10 @@ const Register = () => {
     <div className="flex justify-center min-h-screen">
       <div className="w-full md:max-w-screen-2xl md:p-9 p-4 flex flex-col">
         <AppHeader theme="dark" />
-        <div className="flex-grow flex items-center justify-center mt-16 md:mt-0">
+        <form
+          className="flex-grow flex items-center justify-center mt-16 md:mt-0"
+          onSubmit={handleSubmit(onSubmit)}
+        >
           <div className="w-full md:w-9/12">
             <p className="text-2xl animate__animated animate__fadeInLeft">
               New User
@@ -68,79 +75,215 @@ const Register = () => {
             </p>
             <div className="text-sm text-dark mt-2 animate__animated animate__fadeInUp animate__delay-4s">
               <div className="md:flex mt-10 md:space-x-5">
-                <input
-                  type="text"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="First Name *"
-                  value={firstName}
-                  onChange={e => setFirstName(e.target.value)}
-                />
-                <input
-                  type="text"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Last Name *"
-                  value={lastName}
-                  onChange={e => setLastName(e.target.value)}
-                />
-                <input
-                  type="text"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Company/Organization *"
-                  value={company}
-                  onChange={e => setCompany(e.target.value)}
-                />
-              </div>
-              <div className="md:flex mt-5 md:space-x-5">
-                <input
-                  type="email"
-                  className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Email *"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                />
-                <input
-                  type="email"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Confirm Email *"
-                  value={confirmEmail}
-                  onChange={e => setConfirmEmail(e.target.value)}
-                />
+                <div className="flex-1 flex-col">
+                  <input
+                    type="text"
+                    className="w-full mt-2 md:mt-0 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="First Name *"
+                    name="firstName"
+                    {...register('firstName', {
+                      required: 'First name is required',
+                      pattern: {
+                        message: 'First name is invalid',
+                        value: NAME_PATTERN,
+                      },
+                    })}
+                  />
+                  {formState.errors?.firstName && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.firstName?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex-1 flex-col">
+                  <input
+                    type="text"
+                    className="w-full mt-2 md:mt-0 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Last Name *"
+                    name="lastName"
+                    {...register('lastName', {
+                      required: 'Last name is required',
+                      pattern: {
+                        message: 'Last name is invalid',
+                        value: NAME_PATTERN,
+                      },
+                    })}
+                  />
+                  {formState.errors?.lastName && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.lastName?.message}
+                    </p>
+                  )}
+                </div>
                 <div className="flex-1 h-0 md:h-14 px-7" />
               </div>
               <div className="md:flex mt-5 md:space-x-5">
-                <input
-                  type="password"
-                  className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Password *"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-                <input
-                  type="password"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Confirm Password *"
-                  value={confirmPassword}
-                  onChange={e => setConfirmPassword(e.target.value)}
-                />
+                <div className="flex-1 flex-col">
+                  <input
+                    type="text"
+                    className="w-full h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Email *"
+                    name="email"
+                    {...register('email', {
+                      required: 'Email is required',
+                      pattern: {
+                        message: 'Email is invalid',
+                        value: EMAIL_PATTERN,
+                      },
+                      validate: value =>
+                        value === getValues().confirmEmail || 'Email not match',
+                    })}
+                  />
+                  {formState.errors?.email && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.email?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex-1 flex-col">
+                  <input
+                    type="text"
+                    className="w-full mt-2 md:mt-0 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Confirm Email *"
+                    name="confirmEmail"
+                    {...register('confirmEmail', {
+                      required: 'Email Confirm is required',
+                      pattern: {
+                        message: 'Email Confirm is invalid',
+                        value: EMAIL_PATTERN,
+                      },
+                      validate: value =>
+                        value === getValues().email
+                          ? clearErrors('email')
+                          : setError('email', {
+                              message: 'Email not match',
+                            }),
+                    })}
+                  />
+                  {formState.errors?.confirmEmail && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.confirmEmail?.message}
+                    </p>
+                  )}
+                </div>
                 <div className="flex-1 h-0 md:h-14 px-7" />
               </div>
               <div className="md:flex mt-5 md:space-x-5">
-                <input
-                  type="text"
-                  className="w-full md:w-auto flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Forum Name / Pseudonym *"
-                  value={forumName}
-                  onChange={e => setForumName(e.target.value)}
-                />
-                <input
-                  type="text"
-                  className="w-full md:w-auto mt-2 md:mt-0 flex-1 h-14 px-7 rounded-full shadow-md focus:outline-none"
-                  placeholder="Telegram"
-                  value={telegram}
-                  onFocus={e => focusTelegram(e)}
-                  onBlur={e => blurTelegram(e)}
-                  onChange={e => handleTelegram(e.target.value)}
-                />
+                <div className="flex-1 flex-col">
+                  <input
+                    type="password"
+                    className="w-full h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Password *"
+                    name="password"
+                    {...register('password', {
+                      required: 'Password is required',
+                      minLength: {
+                        message: 'Min 8 character',
+                        value: 8,
+                      },
+                      pattern: {
+                        message: 'Password is invalid',
+                        value: PASSWORD_PATTERN,
+                      },
+                      validate: value =>
+                        value === getValues().confirmPassword ||
+                        'Password not match',
+                    })}
+                  />
+                  {formState.errors?.password && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.password?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex-1 flex-col">
+                  <input
+                    type="password"
+                    className="w-full mt-2 md:mt-0 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Confirm Password *"
+                    name="confirmPassword"
+                    {...register('confirmPassword', {
+                      required: 'Confirm password is required',
+                      minLength: {
+                        message: 'Min 8 character',
+                        value: 8,
+                      },
+                      pattern: {
+                        message: 'Confirm password is invalid',
+                        value: PASSWORD_PATTERN,
+                      },
+                      validate: value =>
+                        value === getValues().password
+                          ? clearErrors('password')
+                          : setError('password', {
+                              message: 'Password not match',
+                            }),
+                    })}
+                  />
+                  {formState.errors?.confirmPassword && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.confirmPassword?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex-1 h-0 md:h-14 px-7" />
+              </div>
+              <div className="md:flex mt-5 md:space-x-5">
+                <div className="flex-1 flex-col">
+                  <input
+                    type="text"
+                    className="w-full h-14 px-7 rounded-full shadow-md focus:outline-none"
+                    placeholder="Forum Name / Pseudonym *"
+                    name="forumName"
+                    {...register('forumName', {
+                      required: 'Forum name is required',
+                      pattern: {
+                        message: 'Forum name is invalid',
+                        value: FORUM_PATTERN,
+                      },
+                    })}
+                  />
+                  {formState.errors?.forumName && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.forumName?.message}
+                    </p>
+                  )}
+                </div>
+                <div className="flex-1 flex-col">
+                  <Controller
+                    name="telegram"
+                    control={control}
+                    rules={{
+                      pattern: {
+                        message: 'Telegram is invalid',
+                        value: TELEGRAM_PATTERN,
+                      },
+                    }}
+                    render={({ field: { value, onChange, onBlur } }) => (
+                      <input
+                        type="text"
+                        className="w-full mt-2 md:mt-0 h-14 px-7 rounded-full shadow-md focus:outline-none"
+                        placeholder="Telegram"
+                        name="telegram"
+                        value={value}
+                        onChange={e => {
+                          onChangeTelegram(e);
+                          onChange(e);
+                        }}
+                        onBlur={e => {
+                          blurTelegram(e);
+                          onBlur(e);
+                        }}
+                        onFocus={e => focusTelegram(e)}
+                      />
+                    )}
+                  />
+                  {formState.errors?.telegram && (
+                    <p className="pl-7 mt-2 text-primary">
+                      {formState.errors.telegram?.message}
+                    </p>
+                  )}
+                </div>
                 <div className="flex-1 h-0 md:h-14 px-7" />
               </div>
             </div>
@@ -188,17 +331,16 @@ const Register = () => {
             <div className="md:flex md:flex-row-reverse mt-10">
               <div className="animate__animated animate__fadeInLeft animate__delay-8s">
                 <button
-                  type="button"
+                  type="submit"
                   className="text-lg text-white w-full md:w-64 h-16 rounded-full bg-primary hover:opacity-40 disabled:opacity-40 disabled:cursor-not-allowed focus:outline-none shadow-md"
                   disabled={!validateFields()}
-                  onClick={() => router.push('/welcome')}
                 >
                   Submit
                 </button>
               </div>
             </div>
           </div>
-        </div>
+        </form>
         <AppFooter theme="dark" />
       </div>
     </div>
