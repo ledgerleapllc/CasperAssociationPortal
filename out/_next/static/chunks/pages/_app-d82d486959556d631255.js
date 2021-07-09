d8fd3aafa52cab5cc4dd40168d5dd8dfe838eced
ledgerleap2 <alex@ledgerleap.com>
2021-07-09 04:34:45 -0400

    build

---
+++ out/_next/static/chunks/pages/_app-d82d486959556d631255.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[2888],{1780:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return u(9148)}])}},function(n){var _=function(_){return n(n.s=_)};n.O(0,[9774,1282,8930,845,3345,5073],(function(){return _(1780),_(2441)}));var u=n.O();_N_E=u}]);
