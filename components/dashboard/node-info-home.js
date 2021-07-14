cc1e633cf4a0c269e6a6a44ab2eb0875559fb15c
ledgerleap2 <alex@ledgerleap.com>
2021-07-14 15:13:17 -0400

    CAMP-48

--- components/dashboard/node-info-home.js
+++ components/dashboard/node-info-home.js
@@ -7,47 +7,47 @@ const NodeInfoHome = () => {
   const percenPerformance = 83;
   return (
     <div className="flex flex-col pt-5 lg:pb-3">
-      <span className="text-3xl">Node Info</span>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <span className="text-2xl">Node Info</span>
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Node Rank</span>
+          <span className="text-lg">Node Rank</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
-        <span className="text-xl text-black1 font-thin">8</span>
+        <span className="text-base text-black1 font-thin">8</span>
       </div>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Stake Amount</span>
+          <span className="text-lg">Stake Amount</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
-        <span className="text-xl text-black1 font-thin">2,502,815</span>
+        <span className="text-base text-black1 font-thin">2,502,815</span>
       </div>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Delegators</span>
+          <span className="text-lg">Delegators</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
-        <span className="text-xl text-black1 font-thin">8</span>
+        <span className="text-base text-black1 font-thin">8</span>
       </div>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Uptime</span>
+          <span className="text-lg">Uptime</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
         <p className="text-sm text-gray mb-2">{`Average: {X}%`}</p>
         <ProgressBar percent={75} />
       </div>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Block Height</span>
+          <span className="text-lg">Block Height</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
         <p className="text-sm text-gray mb-2">Current: 1 block behind</p>
         <ProgressBar counts={504} totalCounts={505} type="count" />
       </div>
-      <div className="flex flex-col py-2 2xl:py-3">
+      <div className="flex flex-col py-2">
         <div className="flex flex-row">
-          <span className="text-xxl">Update Responsiveness</span>
+          <span className="text-lg">Update Responsiveness</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
         <p className="text-sm text-gray mb-2">Average: 2+ days early</p>
@@ -60,7 +60,7 @@ const NodeInfoHome = () => {
       </div>
       <div className="flex flex-col pt-2 pb-9 border-b-2 border-gray">
         <div className="flex flex-row">
-          <span className="text-xxl">Peers</span>
+          <span className="text-lg">Peers</span>
           <img className="pl-3" src="/images/ic_feather_info.svg" alt="Info" />
         </div>
         <p className="text-sm text-gray mb-2">Average: 63</p>
@@ -69,7 +69,7 @@ const NodeInfoHome = () => {
       <div className="flex justify-between py-9">
         <div className="flex w-1/2 pr-2 flex-col">
           <div className="flex flex-row pb-4">
-            <span className="text-md font-medium">CPU Load</span>
+            <span className="text-sm font-medium">CPU Load</span>
             <img
               className="pl-2"
               src="/images/ic_feather_info.svg"
@@ -92,7 +92,7 @@ const NodeInfoHome = () => {
         </div>
         <div className="flex w-1/2 pl-2 flex-col">
           <div className="flex flex-row pb-4">
-            <span className="text-md font-medium">Performance</span>
+            <span className="text-sm font-medium">Performance</span>
             <img
               className="pl-2"
               src="/images/ic_feather_info.svg"
