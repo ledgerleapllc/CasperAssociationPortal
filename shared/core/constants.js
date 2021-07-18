a5dc0ca8adfacab811781251758d2c205e680a5a
ledgerleap2 <alex@ledgerleap.com>
2021-07-18 09:51:07 -0400

    shutfipro api key for prod

--- shared/core/constants.js
+++ shared/core/constants.js
@@ -1,8 +1,8 @@
 export const SHUFTI_CONST = {
   production: {
-    clientId: 'KgeOivDVEpzTrAqkx8aBwLJenCgKnW4SSQDbv17hRq8fyhvZhD1612459148',
+    clientId: 'e4tot3Y50imDSYYsVcWQbpQqG4jmZ7i7XoblVDp3CavbFn9Tvr1613403633',
     clientSecret:
-      '$2y$10$wHWfyqB/1dhfTKwQQnstv.k0y9z2gYHBhkkRMkBPtPPOpYODHi6l6',
+      '$2y$10$f5Q.YTY6bl2wvmbS.aT8pONrDSxieRzmzQQdH.2VJkFdj7cRaR05i',
   },
   development: {
     clientId: 'KgeOivDVEpzTrAqkx8aBwLJenCgKnW4SSQDbv17hRq8fyhvZhD1612459148',
