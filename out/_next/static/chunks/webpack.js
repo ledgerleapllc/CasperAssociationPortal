bdbc1bf40c3a0215e73e43944d09c422ee43f975
ledgerleap2 <alex@ledgerleap.com>
2021-07-18 14:03:38 -0400

    build

--- out/_next/static/chunks/webpack.js
+++ out/_next/static/chunks/webpack.js
@@ -182,7 +182,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9b9d627e8f21f39ec86b"; }
+/******/ 		__webpack_require__.h = function() { return "a576b600259f0fc30609"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
