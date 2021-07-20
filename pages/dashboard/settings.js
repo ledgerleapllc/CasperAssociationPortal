c48647f6f484a66a79e13a8f323df4f2d48822ff
ledgerleap2 <alex@ledgerleap.com>
2021-07-20 13:42:34 -0400

    fix spacing

--- pages/dashboard/settings.js
+++ pages/dashboard/settings.js
@@ -24,7 +24,7 @@ const DashboardSetting = () => {
           <div className="card-header lg:mr-24 lg:h-70px">
             <div className="flex justify-between items-end mb-3.5">
               <div className="flex items-center">
-                <h3 className="text-dark2 text-xl font-medium mr-20">
+                <h3 className="text-dark2 text-xl font-medium mr-24">
                   <Link href="/dashboard/settings">
                     <a>User Settings</a>
                   </Link>
