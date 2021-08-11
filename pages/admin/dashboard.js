ac8fe83f654be55a46c9a17c0f62c39738ab8408
ledgerleap2 <alex@ledgerleap.com>
2021-08-11 13:20:57 -0400

    fix membership

--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -7,10 +7,10 @@ import { LoadingScreen } from '../../components/hoc/loading-screen';
 const AdminDashboard = () => (
   <LayoutDashboard>
     <div className="flex gap-5 h-auto lg:h-full">
-      <div className="w-9/12">
+      <div className="w-4/5">
         <ContentAdminHome />
       </div>
-      <Card className="hidden lg:block h-full w-3/12">
+      <Card className="hidden lg:block h-full w-1/5">
         <div className="overflow-y-scroll h-full w-80">
           <InfoRightAdminHome />
         </div>
--- pages/dashboard/perks/index.js
+++ pages/dashboard/perks/index.js
@@ -5,169 +5,26 @@ import { Card } from '../../../components/partials';
 
 const DashboardMemberPerks = () => (
   <LayoutDashboard>
-    <div className="bg-transparent h-full">
-      <div className="h-3/5 w-full">
-        <div className="lg:mr-card lg:h-70px flex flex-col justify-center">
-          <h3 className="text-dark2 text-xl lg:pr-32 font-medium mb-3.5">
-            Member Perks
-          </h3>
-          <div className="border-primary border-b-2" />
-        </div>
-        <div className="h-full lg:pr-card">
-          <div className="flex pb-5 h-11/20">
-            <div className="w-9/20 pr-2.5">
-              <Card className="p-6 lg:shadow-2xl h-full" noShadow>
-                <div className="flex mb-10">
-                  <p className="text-lg">Membership Status</p>
-                  <img
-                    className="pl-3 lg:pl-5"
-                    src="/images/ic_feather_info.svg"
-                    alt="Info"
-                  />
-                </div>
-                <p className="text-xl">MEMBER</p>
-              </Card>
-            </div>
-            <div className="w-9/20">
-              <Card className="p-6 lg:shadow-2xl h-full" noShadow>
-                <div className="h-full flex flex-col justify-between">
-                  <div className="flex flex-col">
-                    <div className="flex flex-row py-1">
-                      <span className="text-lg">Uptime</span>
-                      <img
-                        className="pl-3"
-                        src="/images/ic_feather_info.svg"
-                        alt="Info"
-                      />
-                    </div>
-                    <div className="relative h-4 text-xs">
-                      <div className="overflow-hidden w-full rounded-lg bg-gray flex bg-opacity-50">
-                        <div className="w-3/4 shadow-none flex flex-col text-center whitespace-nowrap text-white font-thin justify-center bg-primary">
-                          75%
-                        </div>
-                      </div>
-                      <div className="transform -translate-y-2/4 absolute h-8 border-l-4 left-20 top-1/2" />
-                    </div>
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="flex flex-row py-1">
-                      <span className="text-lg">Peers</span>
-                      <img
-                        className="pl-3"
-                        src="/images/ic_feather_info.svg"
-                        alt="Info"
-                      />
-                    </div>
-                    <div className="relative h-4 text-xs">
-                      <div className="overflow-hidden w-full rounded-lg bg-gray flex bg-opacity-50">
-                        <div className="w-3/4 shadow-none flex flex-col text-center whitespace-nowrap text-white font-thin justify-center bg-primary">
-                          75%
-                        </div>
-                      </div>
-                      <div className="transform -translate-y-2/4 absolute h-8 border-l-4 left-60 top-1/2" />
-                    </div>
-                  </div>
-                  <div className="flex flex-col">
-                    <div className="flex flex-row py-1">
-                      <span className="text-lg">Performance</span>
-                      <img
-                        className="pl-3"
-                        src="/images/ic_feather_info.svg"
-                        alt="Info"
-                      />
-                    </div>
-                    <div className="relative h-4 text-xs">
-                      <div className="overflow-hidden w-full rounded-lg bg-gray flex bg-opacity-50">
-                        <div className="w-3/4 shadow-none flex flex-col text-center whitespace-nowrap text-white font-thin justify-center bg-primary">
-                          75%
-                        </div>
-                      </div>
-                      <div className="transform -translate-y-2/4 absolute h-8 border-l-4 left-40 top-1/2" />
-                    </div>
-                  </div>
-                </div>
-              </Card>
-            </div>
-          </div>
-          <div className="flex">
-            <div className="w-4/6 pr-2.5">
-              <Card className="p-6 lg:shadow-2xl flex" noShadow>
-                <div className="flex flex-col w-1/2 px-5 lg:px-0 border-r border-gray lg:pl-20 justify-center">
-                  <div className="flex flex-row">
-                    <span className="text-lg">Daily Earnings</span>
-                    <img
-                      className="pl-3 lg:pl-5"
-                      src="/images/ic_feather_info.svg"
-                      alt="Info"
-                    />
-                  </div>
-                  <div className="flex flex-row mt-3">
-                    <img
-                      width="18px"
-                      height="18px"
-                      src="/images/ic_logo_home.svg"
-                      alt="Info"
-                    />
-                    <span className="text-base font-thin pl-3">0.0154</span>
-                  </div>
-                </div>
-                <div className="flex flex-col px-5 lg:px-0 w-1/2 border-r border-gray lg:pl-20 justify-center">
-                  <div className="flex flex-row">
-                    <span className="text-lg">Total Earnings</span>
-                    <img
-                      className="pl-3 lg:pl-5"
-                      src="/images/ic_feather_info.svg"
-                      alt="Info"
-                    />
-                  </div>
-                  <div className="flex flex-row mt-3">
-                    <img
-                      width="18px"
-                      height="18px"
-                      src="/images/ic_logo_home.svg"
-                      alt="Info"
-                    />
-                    <span className="text-base font-thin pl-3">6.101297</span>
-                  </div>
-                </div>
-                <div className="flex flex-col px-5 lg:px-0 w-1/2 lg:pl-20 justify-center">
-                  <div className="flex flex-row">
-                    <span className="text-lg">Total Earnings</span>
-                    <img
-                      className="pl-3 lg:pl-5"
-                      src="/images/ic_feather_info.svg"
-                      alt="Info"
-                    />
-                  </div>
-                  <div className="flex flex-row mt-3">
-                    <img
-                      width="18px"
-                      height="18px"
-                      src="/images/ic_logo_home.svg"
-                      alt="Info"
-                    />
-                    <span className="text-base font-thin pl-3">6.101297</span>
-                  </div>
-                </div>
-              </Card>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="flex flex-col h-2/5 w-full">
+    <Card className="h-full px-card py-7 lg:shadow-2xl" noShadow>
+      <div className="flex flex-col h-full w-full">
         <div className="lg:mr-card lg:h-40px flex flex-col justify-center">
           <h3 className="text-dark2 text-xl lg:pr-32 font-medium mb-2">
             Member Perks
           </h3>
           <div className="border-primary border-b-2" />
         </div>
-        <div className="lg:h-100%-40px">
-          <div className="lg:pr-card flex h-full">
+        <div className="flex flex-col lg:h-100%-40px">
+          <p className="py-4">
+            Welcome Casper Association member. The following perks are available
+            to you. Click any to learn more. Thanks for being a member in good
+            standing with the association.
+          </p>
+          <div className="flex-1 min-h-0 lg:pr-card flex">
             <PerksPreview />
           </div>
         </div>
       </div>
-    </div>
+    </Card>
   </LayoutDashboard>
 );
 
