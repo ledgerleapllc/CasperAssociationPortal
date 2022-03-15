54805039cda06e10ac48c0b6d88fe2fb7c250c67
Ledgerleap <jin@ledgerleap.com>
2022-03-15 10:15:27 -0400

    Env Files

--- components/onboard/submit-kyc/shuftipro.js
+++ components/onboard/submit-kyc/shuftipro.js
@@ -37,7 +37,7 @@ export const Shuftipro = () => {
 
     const payload = {
       reference: referenceIdTemp,
-      callback_url: 'https://backend.caspermember.com/api/shuftipro-status',
+      callback_url: `${process.env.BASE_URL}/api/shuftipro-status`,
       email: authUser.fullInfo.email,
       country: '',
       verification_mode: 'image_only',
